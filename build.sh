922ed80a565b64225826568092fa619df6152072
Bantoniof <bantoniof@libreoffice.org>
2025-06-11 22:13:59 +0200

    v1.1.2 test

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
  
-# This file update the information of versions
-# and compress the extension
+# This file update the information of versions in description.xml
+# and  the update information.xml
+# and build (compress) the extension
 
 Ext="SanityCheck"
 RotExt="SanityCheck"
@@ -9,8 +10,8 @@ RotExt="SanityCheck"
 if [[ ! $1 ]]
 then
 	echo "Usage:"
-	echo "./versionar.sh version"
-	echo "example: ./versionar.sh 12.2.3"
+	echo "./build.sh [version]"
+	echo "example: ./build.sh 12.2.3"
 	exit
 	else
 	echo
@@ -18,6 +19,7 @@ then
    cd $RotExt
 	sed -i s'/<version value.*/<version value="'$1'"\/>/' description.xml
     echo "### Compiling " $Ext
+    # I use 7zip but you can use zip:
     # zip -r SanityCheck.oxt .
      7z a ./$Ext.oxt . -tzip
      cd ..
@@ -25,5 +27,6 @@ then
     echo 
     echo "### updating " $Ext".update.xml"
 	sed -i s'/<version value.*/<version value="'$1'"\/>/' $Ext.update.xml
+	#sed -i s'/raw.*/raw\/'$1'\/'$Ext'.oxt"\/>/' $Ext.update.xml
     echo "### Done"
 fi
