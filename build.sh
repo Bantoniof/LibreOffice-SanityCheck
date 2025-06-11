f370a84b8f87905e097535c46b547dbdd72e72c9
Bantoniof <bantoniof@libreoffice.org>
2025-06-11 10:17:14 +0200

    v1.1.2

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+ 
+# This file update the information of versions
+# and compress the extension
 
-cd SanityCheck
-zip -r SanityCheck.oxt .
-cd ..
-mv SanityCheck/SanityCheck.oxt ./
+Ext="SanityCheck"
+RotExt="SanityCheck"
+
+if [[ ! $1 ]]
+then
+	echo "Usage:"
+	echo "./versionar.sh version"
+	echo "example: ./versionar.sh 12.2.3"
+	exit
+	else
+	echo
+    echo "### updating " $RotExt"/description.xml"
+   cd $RotExt
+	sed -i s'/<version value.*/<version value="'$1'"\/>/' description.xml
+    echo "### Compiling " $Ext
+    # zip -r SanityCheck.oxt .
+     7z a ./$Ext.oxt . -tzip
+     cd ..
+    mv $RotExt/$Ext.oxt ./
+    echo 
+    echo "### updating " $Ext".update.xml"
+	sed -i s'/<version value.*/<version value="'$1'"\/>/' $Ext.update.xml
+    echo "### Done"
+fi
